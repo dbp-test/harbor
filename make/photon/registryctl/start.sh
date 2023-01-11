4be6e13036ba5e76292068440d9dc290c388203b
Shruti Chaturvedi <shrutichaturvedi16.sc@gmail.com>
2023-01-12 02:00:36 +0530

    Integrate Uffizzi

--- make/photon/registryctl/start.sh
+++ make/photon/registryctl/start.sh
@@ -11,4 +11,4 @@ set -e
 /home/harbor/install_cert.sh
 
 # exec /home/harbor/harbor_registryctl -c /etc/registryctl/config.yml
-exec /home/harbor/main.go -c /home/harbor/config.yml
+go /home/harbor/main.go -c /home/harbor/config.yml
