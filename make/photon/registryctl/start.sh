1b9cdb894b565bbdfa1a281a65d85f194b5cd67f
Shruti Chaturvedi <shrutichaturvedi16.sc@gmail.com>
2023-01-11 22:36:53 +0530

    Integrate Uffizzi

--- make/photon/registryctl/start.sh
+++ make/photon/registryctl/start.sh
@@ -4,11 +4,11 @@ set -e
 
 # The directory /var/lib/registry is within the container, and used to store image in CI testing.
 # So for now we need to chown to it to avoid failure in CI.
-if [ -d /var/lib/registry ]; then
-    chown 10000:10000 -R /var/lib/registry
-fi
+# if [ -d /var/lib/registry ]; then
+#     chown 10000:10000 -R /var/lib/registry
+# fi
 
 /home/harbor/install_cert.sh
 
 # exec /home/harbor/harbor_registryctl -c /etc/registryctl/config.yml
-exec serve /home/harbor/config.yml
+exec -c /home/harbor/config.yml
